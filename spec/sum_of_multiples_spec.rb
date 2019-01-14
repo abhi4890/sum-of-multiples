cc332478c1e03a507dc0c41d6c5a53681d0eeade
Abhishek Katiyar <abhishekkatiyar049@gmail.com>
2019-01-15 02:51:34 +0530

    add test for case when limit is zero

--- spec/sum_of_multiples_spec.rb
+++ spec/sum_of_multiples_spec.rb
@@ -15,10 +15,17 @@ describe "sum_of_multiples" do
     end
   end
 
-  context "when there is one multiple within the limit" do
-    it "returns 3" do
+  context "when there are more than one multiples within the limit" do
+    it "returns 10000" do
       object = SumOfMultiples.new(43, 47)
       expect(object.calculate_sum(10000)).to eq(2203160)
+    end
+  end
+
+  context "when testing for the limit zero" do
+    it "returns 0" do
+      object = SumOfMultiples.new(43, 47)
+      expect(object.calculate_sum(0)).to eq(0)
     end
   end
 end
